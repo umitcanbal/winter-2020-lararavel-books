cbdb1cffc5c55d32b5bbad1faa410c155902dfd6
Slavo Kozar <slavo.kozar@gmail.com>
2020-02-26 12:02:59 +0100

    migrations added

---
+++ database/migrations/2020_02_26_102200_create_reviews_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('review');
+            $table->string('name');
+            $table->timestamps(); //created_at and updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
